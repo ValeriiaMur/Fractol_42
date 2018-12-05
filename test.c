73fc0234587a11da3065681af30140383629d2c6
ValeriiaMur <41483072+ValeriiaMur@users.noreply.github.com>
2018-12-04 16:06:48 -0800

    mandelbrot looks better. header, make, julia

--- test.c
+++ test.c
@@ -6,7 +6,7 @@
 /*   By: vmuradia <vmuradia@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/30 17:54:49 by vmuradia          #+#    #+#             */
-/*   Updated: 2018/12/03 22:02:04 by vmuradia         ###   ########.fr       */
+/*   Updated: 2018/12/04 16:05:47 by vmuradia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,20 +15,12 @@
 #include <math.h>
 #include "./libft/libft.h"
 #include "./minilibx/mlx.h"
-#include <tgmath.h>
-
-#define WINDOW	1200
 
 typedef struct	s_data
 {
 	void		*mlx_ptr;
 	void		*win_ptr;
-	void		*img_get_data;
-	void		*img_ptr;
 	int			fractol;
-	int			*bits_p_pixel;
-	int			*endian;
-	int			*size_line;
 	int			color;
 	int			width;
 	int			height;
@@ -36,6 +28,7 @@ typedef struct	s_data
 	double 		maxReal;
 	double		min_i;
 	double		max_i;
+	double 		zoom;
 }				t_data;
 
 void mandelbrot(t_data *data);
@@ -48,11 +41,12 @@ void mandelbrot_init(t_data *data)
 {
 	data->width = 1000; //width
 	data->height = 1000; //height
-	data->minReal = -2.0;
-	data->maxReal = 1.0;
-	data->min_i = -1.0;
-	data->max_i = 1.0;
+	data->minReal = -2.05;
+	data->maxReal = 1.2;
+	data->min_i = -1.3;
+	data->max_i = 1.2;
 	data->color = 0xd7afd7;
+	data->zoom = 300;
 	mandelbrot(data);
 }
 
@@ -61,7 +55,7 @@ int findN(double cr, double ci, double max_n)
 	int i = 0;
 	double zr = 0.0;
 	double zi = 0.0;
-	while ( i < max_n && zr * zr + zi * zi < 4.0)
+	while (i < max_n && zr * zr + zi * zi < 4.0)
 	{
 		double temp = zr * zr - zi * zi + cr;
 		zi = 2.0 * zr * zi + ci;
@@ -71,32 +65,71 @@ int findN(double cr, double ci, double max_n)
 	return (i);
 }
 
-double mapToReal(int x, t_data *data)
+void	ft_zoom(int x, int y, t_data *data, double max_n)
+{
+	data->minReal = (x / data->zoom + data->minReal) - (x / (data->zoom * 1.2));
+	data->min_i = (y / data->zoom + data->min_i) - (y / (data->zoom * 1.2));
+	data->zoom *= 1.2;
+	max_n++;
+}
+
+void	ft_dezoom(int x, int y, t_data *data, double max_n)
+{
+	data->minReal = (x / data->zoom + data->minReal)  - (x / (data->zoom / 1.2));
+	data->min_i = (y / data->zoom + data->min_i) - (y / (data->zoom / 1.2));
+	data->zoom /= 1.2;
+	max_n--;
+}
+
+int		mouse_hook(int mousecode, int x, int y, t_data *data, int max_n)
+{
+	if (mousecode == 5 || mousecode == 2)
+		ft_zoom(x, y, data, max_n);
+	else if (mousecode == 4 || mousecode == 1)
+		ft_dezoom(x, y, data, max_n);
+	mandelbrot(data);
+	return (0);
+}
+
+
+
+double find_real(int x, t_data *data)
 {
 	double range = data->maxReal - data->minReal;
 	return (x * (range /data->width) + data->minReal);
 }
 
-double mapToImaginary(int y, t_data *data)
+double find_imaginary(int y, t_data *data)
 {
 	double range = data->max_i - data->min_i;
 	return (y * (range / data->height) + data->min_i);
 }
 
+
 void mandelbrot(t_data *data)
 {
-	double	max_n = 1000;
-	for (int x = 0; x < data->width; x++)
+	int color;
+	double	max_n = 100;
+
+	mlx_string_put(data->mlx_ptr, data->win_ptr, 500, 40, 0xffffff, "Mandelbrot Set");
+	for (int x = 100; x < data->width; x++)
 	{
-		for (int y = 0; y < data->height; y++)
+		for (int y = 100; y < data->height; y++)
 		{
-			double cr = mapToReal(x, data);
-			double ci = mapToImaginary(y, data);
+			double cr = find_real(x, data);
+			double ci = find_imaginary(y, data);
 			int n = findN(cr, ci, max_n);
-			mlx_pixel_put(data->mlx_ptr, data->win_ptr, x, y, data->color * n);
+			if (n == max_n)
+				mlx_pixel_put(data->mlx_ptr, data->win_ptr, x, y, 0x000000);
+			else if (n > 0 && n < max_n / 2-1)
+				mlx_pixel_put(data->mlx_ptr, data->win_ptr, x, y, (data->color * n / 100));
+			else if (n > max_n/2 && n < max_n - 1)
+				mlx_pixel_put(data->mlx_ptr, data->win_ptr, x, y, (data->color * n / 100));
 		}
 	}
 }
+
+
 
 void fractol_init(t_data *data)
 {
@@ -129,11 +162,18 @@ int fract_select(char **argv, t_data *data)
 	return (1);
 }
 
-int		key_press(int key, void *param)
+int keys(int key, t_data *data)
 {
-	(void)param;
 	if (key == 53)
 		exit(1);
+	if (key == 18)
+		data->color = 0x00ff00;
+	else if (key == 19)
+		data->color = 0xff69b4;
+	else if (key == 20)
+		data->color = 0xffdab9;
+	mlx_clear_window(data->mlx_ptr, data->win_ptr);
+	mandelbrot(data);
 	return (0);
 }
 
@@ -145,14 +185,11 @@ int main(int argc, char **argv)
 	{
 		data = (t_data*)malloc(sizeof(t_data));
 		data->mlx_ptr = mlx_init();
-		data->win_ptr = mlx_new_window(data->mlx_ptr, 1000, 1000, "My Fractol");
+		data->win_ptr = mlx_new_window(data->mlx_ptr, 1100, 1100, "My Fractol");
 		if ((fract_select(argv, data)) == 0)
 			return (-1);
-
-		//data->img_ptr = mlx_new_image(data->mlx_ptr, WINDOW, WINDOW);
-	//	data->img_ptr = mlx_get_data_addr(data->img_ptr, data->bits_p_pixel,
-		//data->size_line, data->endian);
-		mlx_key_hook(data->win_ptr, key_press, (void *)0);
+		mlx_key_hook(data->win_ptr, keys, data);
+		mlx_mouse_hook(data->win_ptr, mouse_hook, data);
 		mlx_loop(data->mlx_ptr);
 		free(data);
 	}
